f226d95fa1128ee1a4af9d5fe9950b101b4d8632
iammithun <157653522+iammithun@users.noreply.github.com>
2024-04-25 19:29:14 -0400

    Add files via upload

---
+++ partten2.py
@@ -0,0 +1,13 @@
+total_rows=43int(input("Enter the nembers of rows:"))
+
+for row in  range(1, total_rows+1):
+    
+    
+    for space in range(1, (total_rows-1)+1):
+        print("",end="")
+        
+        
+    for symbol in range(1, row+1):
+        print("*", end="")
+        
+        
